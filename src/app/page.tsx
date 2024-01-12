1565d88047585a8b5eebf8a022bd8cfe66276175
SabbirSami <smd71430@gmail.com>
2024-01-12 18:09:45 +0600

    make banner responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import Banner from "@/components/home/Banner";
 import "../styles/globals.css";
 
 export default function Home() {
-    return <>{/* <Banner /> */}</>;
+    return (
+        <>
+            <Banner />
+        </>
+    );
 }
--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 const Banner = () => {
     return (
         <section className="border-b-4 border-dark">
-            <div className="h-[88vh] container mx-auto hero-bg ">
-                <div className="flex justify-center items-center h-full">
-                    <div className="text-center">
+            <div className="lg:h-[88vh] md:h-[70vh] h-[65vh] container mx-auto hero-bg  ">
+                <div className="flex justify-center items-center h-full md:px-6 px-3">
+                    <div className="lg:pb-0 pb-16 text-center">
                         <div className="">
-                            <h2 className="text-8xl font-bold">Saves Lives.</h2>
-                            <h2 className="text-8xl font-bold text-primary">
+                            <h2 className="xl:text-8xl lg:text-6xl md:text-6xl text-4xl font-bold">
+                                Saves Lives.
+                            </h2>
+                            <h2 className="xl:text-8xl md:text-7xl text-4xl font-bold text-primary">
                                 Blood Donation
                             </h2>
                         </div>
@@ -23,13 +25,13 @@ const Banner = () => {
                         <div className="space-x-3">
                             <Link
                                 href={"/donate"}
-                                className="px-8 py-3 bg-primary rounded-full text-secondary font-semibold"
+                                className="md:px-8 px-5 md:py-3 py-2 md:text-base text-sm border border-primary bg-primary rounded-full text-secondary md:font-semibold"
                             >
                                 Donate
                             </Link>
                             <Link
                                 href={"/donate"}
-                                className="px-8 py-3 border border-primary rounded-full text-dark font-semibold"
+                                className="md:px-8 px-5 md:py-3 py-2 md:text-base text-sm border border-primary rounded-full text-dark md:font-semibold"
                             >
                                 Register Now
                             </Link>
